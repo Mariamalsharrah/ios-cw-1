ff3504e5c13cc4385006780a3647f64a522512b4
Mariamalsharrah <66683233+Mariamalsharrah@users.noreply.github.com>
2020-06-16 22:54:06 +0300

    mariam alsharrah
    done

--- cw1mariamalsharrah580.playground/Contents.swift
+++ cw1mariamalsharrah580.playground/Contents.swift
@@ -10,3 +10,17 @@ var hobby2 = "sleeping"
 print(name1 , age1 , height , hobby1 , hobby2)
 print("my name is \(name1) and  my age is \(age1) ")
 print("my name is name1 ")
+
+var friendName = "Malak"
+var friendAge = 17
+var friendHeight = 167.5
+var friendHobby1 = "sleeping"
+var friendHobby2 = "eating"
+
+var age_difference = 0
+
+(age1) = (friendAge)
+(height) < (friendHeight)
+
+
+
