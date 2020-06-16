7bbac8afe0fab7b196425cd218e0e7f2f5a61186
Mariamalsharrah <66683233+Mariamalsharrah@users.noreply.github.com>
2020-06-16 20:47:44 +0300

    mariam

---
+++ cw1mariamalsharrah580.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+let name1 = "mariam"
+var age1 = 17
+var height = 160.4
+var hobby1 = "swimming "
+var hobby2 = "sleeping"
+
+print(name1 , age1 , height , hobby1 , hobby2)
+print("my name is \(name1) and  my age is \(age1) ")
+print("my name is name1 ")
